7f5c9ef7213d4915e764281d0b760f1492df546c
lukaskelley <lukaskelley412@gmail.com>
2022-05-09 06:16:22 +0300

    I/O project

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // --snip--
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
